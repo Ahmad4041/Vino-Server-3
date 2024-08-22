ce0864d97694f848f15820aec404bbaa0650ab9a
viralcomputers <git@viralcomputers.com>
2024-08-22 13:25:43 +0100

    ********* requestBody/ requestParse fix ******8

--- src/helpers/requestParse.php
+++ src/helpers/requestParse.php
@@ -1,26 +1,43 @@
 <?php
 
+
 function requestParse($request)
 {
     $data = [];
 
+    // Parse query parameters
     $queryParams = $request->getQueryParams();
     if (!empty($queryParams)) {
-        $data['query'] = $queryParams;
+        $data = $queryParams;
     }
 
+    // Get content type and raw body
     $contentType = $request->getHeaderLine('Content-Type');
     $rawBody = $request->getBody()->__toString();
 
-    if (strpos($contentType, 'application/json') !== false) {
-        $data['body'] = json_decode($rawBody, true);
-    } elseif (strpos($contentType, 'application/x-www-form-urlencoded') !== false) {
-        parse_str($rawBody, $parsedBody);
-        $data['body'] = $parsedBody;
-    } elseif (strpos($contentType, 'multipart/form-data') !== false) {
-        $data['body'] = $request->getParsedBody();
-    } else {
-        $data['body'] = $rawBody;
+    // Parse body based on content type
+    if (!empty($rawBody)) {
+        $bodyData = [];
+        if (strpos($contentType, 'application/json') !== false) {
+            $bodyData = json_decode($rawBody, true) ?: [];
+        } elseif (strpos($contentType, 'application/x-www-form-urlencoded') !== false) {
+            parse_str($rawBody, $bodyData);
+        } elseif (strpos($contentType, 'multipart/form-data') !== false) {
+            $bodyData = $request->getParsedBody() ?: [];
+            // Handle file uploads if necessary
+            $files = $request->getUploadedFiles();
+            if (!empty($files)) {
+                foreach ($files as $key => $file) {
+                    $bodyData[$key] = $file;
+                }
+            }
+        } else {
+            // For any other content type, store raw body
+            $bodyData = ['rawBody' => $rawBody];
+        }
+
+        // Merge body data with existing data (query params)
+        $data = array_merge($data, $bodyData);
     }
 
     return $data;
