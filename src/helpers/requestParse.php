61d42036aabd36c5f1e40a0b493efec1106d4e58
salmantariq595 <salmantariq597@gmail.com>
2024-08-22 16:28:24 +0500

    ****** incoming request data handle ******

--- src/helpers/requestParse.php
+++ src/helpers/requestParse.php
@@ -3,5 +3,16 @@
 function requestParse($request)
 {
     $rawBody = $request->getBody()->__toString();
-    return json_decode($rawBody, true);
+    $contentType = $request->getHeaderLine('Content-Type');
+
+    if (strpos($contentType, 'application/json') !== false) {
+        return json_decode($rawBody, true);
+    } elseif (strpos($contentType, 'application/x-www-form-urlencoded') !== false) {
+        parse_str($rawBody, $data);
+        return $data;
+    } elseif (strpos($contentType, 'multipart/form-data') !== false) {
+        return $request->getParsedBody();
+    } else {
+        return $rawBody;
+    }
 }
